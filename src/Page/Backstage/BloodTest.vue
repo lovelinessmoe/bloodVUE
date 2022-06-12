dd3e6592e7a4d00cbd45f069557f36ac0cd4bc1e
孔 <19052102200@qq.com>
2022-06-12 10:33:14 +0800

    删除背景图片

--- src/Page/Backstage/BloodTest.vue
+++ src/Page/Backstage/BloodTest.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="animate" style="margin-top: 80px;padding: 20px;">
-    <div class="login_container"
-         style="background-image: url('https://s1.ax1x.com/2022/06/11/XgZUnU.png');
-                background-repeat: no-repeat;
-                background-size: cover;
-                height: 800px">
+
 
 
       <avue-crud :data="data" :option="option" :table-loading="showLoading" >
@@ -17,7 +13,7 @@
       </avue-crud>
 
     </div>
-  </div>
+
 </template>
 
 <script>
