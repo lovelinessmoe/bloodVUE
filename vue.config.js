e0cc6c13dc14d5d3dc4a768653988f69b7bfeb61
loveliness <1695560542@qq.com>
2022-06-12 10:01:31 +0800

    更改项目端口号，更改默认请求地址

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    devServer: {
-        //vue自己启动的端口
-        port:10791
-    }
 })
